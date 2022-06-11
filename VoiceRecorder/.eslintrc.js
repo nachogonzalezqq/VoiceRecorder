1511780a981e652fdf0fc2ae181f8e48d49e1900
Ignacio Gonzalez Ampudia <ignaciogonzalezampudia@gmail.com>
2022-06-11 15:34:41 -0300

    project creation. eslint and ts installation

---
+++ VoiceRecorder/.eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true,
+		'amd': true,
+		'node': true,
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended'
+	],
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true,
+			'modules': true,
+		},
+		'ecmaVersion': 6,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'always'
+		]
+	}
+};
