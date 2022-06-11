705b2bbe324ad24474d43bc46feb2e79cc397d3c
Ignacio Gonzalez Ampudia <ignaciogonzalezampudia@gmail.com>
2022-06-11 17:54:38 -0300

    install ui library and test debugging

--- VoiceRecorder/src/components/pages/records-list-page/records-list-page.tsx
+++ VoiceRecorder/src/components/pages/records-list-page/records-list-page.tsx
@@ -1,12 +1,19 @@
 import * as React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
+import { Button } from "@react-native-material/core";
 
 interface RecordsListPageProps {}
 
 const RecordsListPage = (props: RecordsListPageProps) => {
+  const onPressDebug = () => {
+    console.log('this is our debug');
+    const test = 'We should see this only using breakpoints';
+    return test;
+  }
   return (
     <View style={styles.container}>
       <Text>RecordsListPage</Text>
+      <Button title="Debug" onPress={onPressDebug} />
     </View>
   );
 };
@@ -14,5 +21,10 @@ const RecordsListPage = (props: RecordsListPageProps) => {
 export default RecordsListPage;
 
 const styles = StyleSheet.create({
-  container: {}
+  container: {
+    display: 'flex',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
 });
